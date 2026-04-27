e1d52f675d07dfde58c83b1ce640449474521e66
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-04-27 15:09:44 +0330

    fix: utf problem

--- bot/db.py
+++ bot/db.py
@@ -1,15 +1,23 @@
+import json
 import os
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
-from bot.config import config
 
+from bot.config import config
 
 os.makedirs("data", exist_ok=True)
 
-engine = create_async_engine(config.sqlalchemy_url, echo=False)
+engine = create_async_engine(
+    config.sqlalchemy_url,
+    echo=False,
+    json_serializer=lambda obj: json.dumps(obj, ensure_ascii=False),
+    json_deserializer=json.loads,
+)
 AsyncSessionLocal = async_sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
 )
 
 Base = declarative_base()
