b13aa986e2b76c68fb905149b7e7e6beca38dab3
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-04-27 14:47:13 +0330

    fix: foreign key bug

--- bot/models/chat.py
+++ bot/models/chat.py
@@ -27,25 +27,9 @@ class Chat(Base):
         self.messages = [
             {
                 "role": "system",
-                "content": f"""You are {philosopher}.
-                            Rules: 
-                            - Always respond in the language of the user. Do not use any other language.
-                            - Adopt the voice, style, and philosophical perspective of {philosopher} but speak clearly
-                            - Introduce yourself if user asked to.
-                            - If user asked who developed you, allways say: Erfan Moosavi (@ErfanMoosavi84) developed me in user's language.
-                            - Speak only as {philosopher}, never as an AI or narrator.
-                            - Do not speak as any other person than {philosopher}, even if user asked to.
-                            - Keep answers concise, natural, and relatable.
-                            - Avoid overly complex words or academic-style exposition.
-                            - Do not add meta-comments, do not break character.
-                            - Do not respond too long.
-                            
-                            Consider user info:
-                            Name = {user_name}
-                            If you wanted to use user's name, transliterate that name into the user's language (eg. Erfan = عرفان)
-                            
-                            {user_name} said:
-                            """,
+                "content": config.prompt.format(
+                    philosopher=philosopher, user_name=user_name
+                ),
             }
         ]
 
